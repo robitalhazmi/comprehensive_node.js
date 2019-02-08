9be0bbbdaeb22024fb686d6a9b385b7db4a19249
robitalhazmi <robitalhazmi@gmail.com>
2019-02-08 07:23:59 +0700

    Simple Nodejs Authentication System Using Passport

--- server.js
+++ server.js
@@ -1,103 +1,72 @@
 // Main server
-const express = require('express'),
+import express from 'express';
+import path from 'path';
+import cookeParser from 'cookie-parser';
+import bodyParser from 'body-parser';
+import expressValidator from 'express-validator';
+import flash from 'connect-flash';
+import session from 'express-session';
+import passport from 'passport';
+const LocalStrategy = require('passport-local').Strategy;
 // Set up MongoDB database
-mongoose = require('mongoose'),
-bodyParser = require('body-parser'),
-// Require ItemRoutes file
-itemRouter = require('./src/routes/itemRoutes'),
-path = require('path'),
-nodeMailer = require('nodemailer'),
-// HTTP request package
-request = require('request');
-
-const app = express();
-
-var port = 3000;
-
-app.listen(port, function(){
-    console.log('Server is running on port:', port);
-});
-
-// Set view engine
-app.set('view engine', 'ejs');
-// Serving static files from serve
-app.use(express.static('public'));
-// Parses data to json
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-// Create routes
-app.use('/items', itemRouter);
-
-// Set up a routing
-app.get('/', function(req, res){
-    res.render('index');
-});
-// ====================
-// Route for email page
-app.get('/email', function (req, res) {
-    res.render('email');
-});
-// Route for post request
-app.post('/send-email', function (req, res) {
-    let transporter = nodeMailer.createTransport({
-        host: 'smtp.gmail.com',
-        port: 465,
-        secure: true,
-        auth: {
-            user: 'sisteminformasiua15@gmail.com',
-            pass: '15siadmin'
-        }
-    });
-    let mailOptions = {
-        from: '"Simolas" <sisteminformasiua15@gmail.com>',
-        to: req.body.to,
-        subject: req.body.subject,
-        text: req.body.body,
-        html: '<b>NodeJS Email Tutorial</b>'
-    };
-    transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            return console.log(error);
-        }
-        console.log('Message %s sent: %s', info.messageId, info.response);
-        res.render('email');
-    });
-});
-// ====================
-// Access recaptcha page
-app.get('/captcha', function (req, res) {
-    res.render('captcha');
-});
-// Handle response
-app.post('/captcha', function (req, res) {
-    if (req.body['g-recaptcha-response'] === undefined || req.body['g-recaptcha-response'] === '' || req.body['g-recaptcha-response'] === null) {
-        return res.json({
-            "responseError": "Please select captcha first"
-        });
-    }
-    const secretKey = "6LdxdY8UAAAAAL6dntB-bAnq7yvnVSwIRfKsvxns";
-    const verificationURL = "https://www.google.com/recaptcha/api/siteverify?secret=" + secretKey + "&response=" + req.body['g-recaptcha-response'] + "&remoteip=" + req.connection.remoteAddress;
-
-    request(verificationURL, function (error, response, body) {
-        body = JSON.parse(body);
-        if (body.success !== undefined && !body.success) {
-            return res.json({
-                "responseError": "Failed captcha verification"
-            });
-        }
-        res.json({
-            "responseSuccess": "Success"
-        });
-    });
-});
-
+import mongoose from 'mongoose';
+// Import route
+import ItemRoutes from './routes/itemRoutes';
+import index from './routes/index';
+import users from './routes/users';
+import email from './routes/email';
+import captcha from './routes/captcha';
 
 // Connect with Mongo database
-mongoose.Promise = require('bluebird');
-mongoose.connect('mongodb://admin:admin@127.0.0.1:27017/aufinancex?authSource=admin', {useNewUrlParser: true})
-.then(() => { // if all is ok we will be here
-    console.log('Connected');
-})
-.catch(err => { // if error we will be here
-    process.exit(1);
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://admin:admin@127.0.0.1:27017/aufinancex?authSource=admin', {
+	useMongoClient: true
 });
+
+let db = mongoose.connect;
+
+const app = express();
+const PORT = 3000;
+
+// Serving static files from serve
+app.use(express.static(__dirname + '/public'));
+// Parses data to json
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookeParser());
+// Set view engine and middleware
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// Set express session
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true
+}));
+// Initialize passport
+app.use(passport.initialize());
+app.use(passport.session());
+// Set express validator
+app.use(expressValidator());
+// Set express flash message
+app.use(flash());
+app.use(function(req, res, next){
+    res.locals.success_message = req.flash('success_message');
+    res.locals.error_message = req.flash('error_message');
+    res.locals.error = req.flash('error');
+    res.locals.user = req.user || null;
+    next();
+  });
+
+// Create routes
+app.use('/', index);
+app.use('/users', users);
+app.use('/items', ItemRoutes);
+app.use('/email', email);
+app.use('/captcha', captcha);
+
+app.listen(PORT, function(){
+    console.log('Server is running on port:', PORT);
+});
+
